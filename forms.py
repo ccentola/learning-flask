0bb56bf530c2d70780d215e1b2fadfd89bac3548
Carl Centola <carl.centola@gmail.com>
2017-12-09 16:18:27 -0500

    Added database to store user data

--- forms.py
+++ forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import Form
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
 
 class SignupForm(Form):
-    first_name = StringField('First Name')
-    last_name = StringField('Last Name')
-    email = StringField('Email')
-    password = PasswordField('Password')
+    first_name = StringField('First Name', validators=[DataRequired("Please enter your first name.")])
+    last_name = StringField('Last Name', validators=[DataRequired("Please enter your last name.")])
+    email = StringField('Email', validators=[DataRequired("Please enter your email."), Email("Please enter a valid email address.")])
+    password = PasswordField('Password', validators=[DataRequired("Please enter a password."), Length(min=6,message="Password must be 6 characters or more." )])
     submit = SubmitField('Sign up')
