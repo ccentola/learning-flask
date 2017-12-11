c1194228d95b98996a7594d2c32cee54619d850a
Carl Centola <carl.centola@gmail.com>
2017-12-11 08:54:51 -0500

    Final app

--- forms.py
+++ forms.py
@@ -13,3 +13,7 @@ class LoginForm(Form):
     email = StringField('Email', validators=[DataRequired("Please enter your email."), Email("Please enter a valid email address.")])
     password = PasswordField('Password', validators=[DataRequired("Please enter a password.")])
     submit = SubmitField('Sign up')
+
+class AddressForm(Form):
+    address = StringField('Address', validators=[DataRequired("Please enter an address.")])
+    submit = SubmitField("Search")
--- routes.py
+++ routes.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, session, redirect, url_for
-from models import db, User
-from forms import SignupForm, LoginForm
+from models import db, User, Place
+from forms import SignupForm, LoginForm, AddressForm
 
 app = Flask(__name__)
 
@@ -20,6 +20,10 @@ def about():
 
 @app.route('/signup', methods=['GET','POST'])
 def signup():
+
+    if 'email' in session:
+        return redirect(url_for('home'))
+
     form = SignupForm()
 
     if request.method == 'POST':
@@ -38,6 +42,10 @@ def signup():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+
+    if 'email' in session:
+        return redirect(url_for('home'))
+
     form = LoginForm()
 
     if request.method == 'POST':
@@ -62,8 +70,34 @@ def logout():
     return redirect(url_for('index'))
 
 
-@app.route('/home')
+@app.route('/home', methods=['GET', 'POST'])
 def home():
+    if 'email' not in session:
+        return redirect(url_for('login'))
+
+    form = AddressForm()
+
+    places = []
+    my_coordinates = (37.4221, -122.0844)
+
+    if request.method == 'POST':
+        if form.validate == False:
+            return render_template('home.html', form=form)
+        else:
+            # get the address
+            address = form.address.data
+
+            # query for places around it
+            p = Place()
+            my_coordinates = p.address_to_latlng(address)
+            places = p.query(address)
+
+            # return those results
+            return render_template('home.html', form=form, my_coordinates=my_coordinates, places=places)
+    
+    elif request.method == 'GET':
+        return render_template('home.html', form=form, my_coordinates=my_coordinates, places=places)
+
     return render_template('home.html')
 
 if __name__ == "__main__":
